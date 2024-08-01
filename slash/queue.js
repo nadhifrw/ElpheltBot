4700c316bea94e15420c3dd01ad010b87d384157
nadhifrw <hernadhif.rawir@gmail.com>
2024-08-01 18:47:38 +0700

    fixing queue

--- slash/queue.js
+++ slash/queue.js
@@ -14,17 +14,19 @@ module.exports = {
 
         if (!queue || !queue.playing) {
             await interaction.reply({ content: "There is no song in queue" });
+        } else {
+            const currentSong = queue.songs[0];
+            const queueString = queue.songs.slice(1).map((song, index) => `${index + 1}. [${song.name}](${song.url})`).join("\n");
+
+            const queueEmbed = {
+                color: 0x0099ff,
+                title: 'Music Queue',
+                description: `**Currently Playing:**\n[${currentSong.name}](${currentSong.url})\n\n**Up Next:**\n${queueString || "No more songs in queue."}`,
+            };
+
+            await interaction.reply({ embeds: [queueEmbed] });
         }
 
-        const currentSong = queue.songs[0];
-        const queueString = queue.songs.slice(1).map((song, index) => `${index + 1}. [${song.name}](${song.url})`).join("\n");
-
-        const queueEmbed = {
-            color: 0x0099ff,
-            title: 'Music Queue',
-            description: `**Currently Playing:**\n[${currentSong.name}](${currentSong.url})\n\n**Up Next:**\n${queueString || "No more songs in queue."}`,
-        };
-
-        await interaction.reply({ embeds: [queueEmbed] });
+        
     }
 }
